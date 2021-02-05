b62bf6fdb8cac924bc94d3b5c2c65725edc5d8f8
stacew <chunrans4@naver.com>
2021-02-06 03:11:02 +0900

    BackTracking

--- src/GenerateParentheses_22.java
+++ src/GenerateParentheses_22.java
@@ -2,28 +2,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses_22 {
-	
-	void BT(StringBuilder str, int count) {
-		if( str.length() == countLP * 2 ) {
-			if( count > countLP )
+
+	void BT(StringBuilder str, int countL, int countR) {
+		if (str.length() == mCountL * 2) {
+			if (countL > mCountL)
 				return;
 			mRet.add(str.toString());
 			return;
 		}
+
+		if (countL < mCountL) {
+			// BT(new StringBuilder(str.toString()).append('('), countL + 1, countR);
+			str.append('(');
+			BT(str, countL + 1, countR);
+			str.setLength(str.length() - 1);
+		}
 		
-		if( count < countLP )
-			BT(new StringBuilder(str.toString()).append('('), count + 1 );
+		if (countL > countR) {
+			// BT(new StringBuilder(str.toString()).append(')'), countL, countR + 1);
+			str.append(')');
+			BT(str, countL, countR + 1);
+			str.setLength(str.length() - 1);
+		}
 		
-		BT(new StringBuilder(str.toString()).append(')'), count );		
 	}
-	
-    public List<String> generateParenthesis(int n) {
-    	countLP = n;
-        mRet = new ArrayList<String>();
-        StringBuilder str = new StringBuilder();
-        BT( str.append('('), 1 );   
-        return mRet;
-    }
-    int countLP;
+
+	public List<String> generateParenthesis(int n) {
+		mCountL = n;
+		mRet = new ArrayList<String>();
+		StringBuilder str = new StringBuilder();
+		BT(str.append('('), 1, 0);
+		return mRet;
+	}
+
+	int mCountL;
 	List<String> mRet;
 }
