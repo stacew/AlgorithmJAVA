ca70f9456f1fe8e97f6e0888ae82fe349fa92889
stacew <chunrans4@naver.com>
2021-02-09 20:58:57 +0900

    주석 추가

--- src/leetcode/linkedlist/RemoveLinkedListElements_203.java
+++ src/leetcode/linkedlist/RemoveLinkedListElements_203.java
@@ -1,4 +1,5 @@
 package leetcode.linkedlist;
+//dummy를 만들고 head 앞에 붙여서 제거 되지 않는 것만 붙이는 방식이 구현 더 쉬움.
 
 public class RemoveLinkedListElements_203 {
     public ListNode removeElements(ListNode head, int val) {
