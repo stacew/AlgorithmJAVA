d50d07febe9961f61d5abed7d3e856e4ab11ab3c
stacew <chunrans4@naver.com>
2021-02-13 00:06:22 +0900

    array, in-place 3type swap

---
+++ src/leetcode/array/SortColors_75.java
@@ -0,0 +1,23 @@
+package leetcode.array;
+//in-place 3type swap
+//빨리 구현하려면 그냥 counting sort..
+//*. 구현 때, 디버깅 많이 필요하면 조건을 확실히 나눠서 처리 
+public class SortColors_75 {
+	public void sortColors(int[] nums) {
+		int i0 = 0, i2 = nums.length - 1;
+		int pivot = 0;
+		while (pivot <= i2) {
+			if (nums[pivot] == 0) {
+				pivot++;
+			} else if (nums[pivot] == 1) {
+				nums[pivot] = nums[i0];
+				nums[i0] = 1;
+				i0++;
+			} else if (nums[pivot] == 2) {
+				nums[pivot] = nums[i2];
+				nums[i2] = nums[pivot];
+				i2--;
+			}
+		}
+	}
+}
