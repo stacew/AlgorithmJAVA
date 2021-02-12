4b47dd9b4f18309526da230308a26f2ba566df78
stacew <chunrans4@naver.com>
2021-02-13 00:51:06 +0900

    java rule of 3 ==

--- src/leetcode/linkedlist/commonDefined.java
+++ src/leetcode/linkedlist/commonDefined.java
@@ -15,4 +15,14 @@ class ListNode {
 		this.val = val;
 		this.next = next;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof ListNode)
+			return false;
+		if (this.val == ((ListNode) o).val && this.next == ((ListNode) o).next)
+			return true;
+		
+		return false;
+	}
 }
