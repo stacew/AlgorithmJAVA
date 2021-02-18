b89f98dd84761e7897d93de936956e112f65cc23
stacew <chunrans4@naver.com>
2021-02-18 21:33:16 +0900

    //1. middle + split +  back reverse + combine ( 1ms ) //2. ArrayList ( 2ms )

---
+++ src/leetcode/linkedlist/ReorderList_143.java
@@ -0,0 +1,61 @@
+package leetcode.linkedlist;
+//1. middle + split +  back reverse + combine ( 1ms )
+//2. ArrayList ( 2ms )
+import java.util.ArrayList;
+public class ReorderList_143 {
+	
+	public void reorderList(ListNode head) {
+	    if( head == null )
+            return;
+	    
+		MiddleOfTheLinkedList_876 sol876 = new MiddleOfTheLinkedList_876();
+		ListNode middle = sol876.middleNode_FastSlow(head);
+		
+		ListNode head2 = middle.next;		
+		middle.next = null;
+		
+		ReverseLinkedList_206 sol206 = new ReverseLinkedList_206();
+		head2 = sol206.reverseList(head2);
+		
+		ListNode head1 = head;
+		head = new ListNode();
+		ListNode cur = head;
+		while( head1 != null ) {
+			cur.next = head1;
+			head1 = head1.next;
+			cur = cur.next;
+			if( head2 == null )
+				break;
+			cur.next = head2;
+			head2 = head2.next;
+			cur = cur.next;						
+		}
+			
+		head = head.next;		
+	}
+	
+	public void reorderList_ArrayList(ListNode head) {
+
+		ArrayList<ListNode> arr = new ArrayList<>();
+		while (head != null) {
+			arr.add(head);
+			head = head.next;
+		}
+
+		int l = 0;
+		int r = arr.size() - 1;
+		head = new ListNode();
+		ListNode cur = head;
+		while (l <= r) {
+			cur.next = arr.get(l++);
+			cur = cur.next;
+			if (l > r)
+				break;
+			cur.next = arr.get(r--);
+			cur = cur.next;
+		}
+		cur.next = null;
+
+		head = head.next;
+	}
+}
