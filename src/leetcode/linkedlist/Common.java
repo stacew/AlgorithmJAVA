ef162e28d2940f6469cd41bc8861a7490d0449ee
stacew <chunrans4@naver.com>
2021-02-20 14:48:09 +0900

    update

--- src/leetcode/linkedlist/Common.java
+++ src/leetcode/linkedlist/Common.java
@@ -1,4 +1,16 @@
 package leetcode.linkedlist;
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
 
 class ListNode {
 	int val;
